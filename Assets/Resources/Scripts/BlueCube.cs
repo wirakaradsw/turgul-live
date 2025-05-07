e20a63eeb5c3a7828e8af3dcd4a5be46468b4e38
wirakaradsw <dynamicsoulworks@gmail.com>
2025-05-08 11:58:42 +1200

    Added Turgul get ready to fight scene
    - Add turgul_getReady scene
    - Add TURGUL 3D text
    - Add get ready intro music
    - Add blue cube and cube spawner

---
+++ Assets/Resources/Scripts/BlueCube.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlueCube : MonoBehaviour {
+
+	public float speed = 1f;
+	
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+		if (transform.position.z < -4f) {
+			Destroy (gameObject);
+		}
+		
+		if (Time.timeScale != 0) {
+			transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z - (speed * Time.deltaTime));
+		}
+	}
+}
---
+++ Assets/Resources/Scripts/CubeSpawner.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeSpawner : MonoBehaviour {
+
+	GameObject blueCube;
+
+	float moveTimer = 0;
+	public float moveTimerMax = 100;
+
+	int randSpawn = 0;
+
+	float randX = 0;
+	float randY = 0;
+	
+	void Start () {
+		
+		moveTimer = moveTimerMax;
+
+		transform.position = new Vector3(Random.Range (-5f, 5f), Random.Range (-5f, 0f), transform.position.z);
+
+		blueCube = Instantiate (Resources.Load ("Prefabs/BlueCube"), new Vector3 (transform.position.x, transform.position.y, transform.position.z), Quaternion.identity) as GameObject;
+		blueCube.transform.Rotate (Random.Range (0f, 360f), Random.Range (0f, 360f), Random.Range (0f, 360f));
+	}
+	
+	void Update () {
+		
+		moveTimer =- Time.deltaTime;
+		
+		if (moveTimer <= 0) {
+			
+			moveTimer = moveTimerMax;
+			
+			randX = Random.Range (-5f, 5f);
+			randY = Random.Range (-5f, 0f);
+			randSpawn = Random.Range (1, 4);
+			
+			transform.position = new Vector3(randX, randY, transform.position.z);
+
+			if (randSpawn == 2){
+
+				blueCube = Instantiate (Resources.Load ("Prefabs/BlueCube"), new Vector3 (transform.position.x, transform.position.y, transform.position.z), Quaternion.identity) as GameObject;
+				blueCube.transform.Rotate (Random.Range (0f, 360f), Random.Range (0f, 360f), Random.Range (0f, 360f));
+			}
+			
+		}
+		
+	}
+}
